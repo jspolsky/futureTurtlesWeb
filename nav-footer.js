73104f16e9566818b4b96d08b379f495db68751d
Joel Spolsky <joel@spolsky.com>
2025-04-04 16:20:36 -0400

    moving common blocks out into scripts

---
+++ nav-footer.js
@@ -0,0 +1,73 @@
+document.write(`
+    			<footer id="footer">
+				<div class="inner">
+					<section>
+						<h2>Stay in touch!</h2>
+						<p>There are lots of opportunities to meet the Turtles and get involved
+							in our on- and off-playa activities.
+						</p>
+						<p>We publish cute pictures on <a href="https://instagram.com/futureturtles">Instagram</a> and
+							hang
+							out on <a href="https://discord.com/invite/rpqSEVWy2Y">Discord</a>. You can email
+							us at <strong>info@futureturtles.com</strong>.</p>
+						<ul class="icons style2">
+							<li><a href="https://instagram.com/futureturtles" class="icon brands fa-instagram"><span
+										class="label">Instagram</span></a></li>
+							<li><a href="https://discord.com/invite/rpqSEVWy2Y" class="icon brands fa-discord"><span
+										class="label">Discord</span></a></li>
+						</ul>
+					</section>
+					<section>
+
+						<p>Sign up for our newsletter to get involved!</p>
+						<div id="mc_embed_signup">
+							<form
+								action="https://futureturtles.us1.list-manage.com/subscribe/post?u=51ad6738951128de8653576d7&amp;id=2abe0abca0&amp;f_id=00566fe2f0"
+								method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
+								class="validate" target="_self">
+
+								<div id="mc_embed_signup_scroll" class="fields">
+
+									<div class="field half">
+										<input type="text" value="" name="FNAME" class="" id="mce-FNAME"
+											placeholder="First Name">
+									</div>
+									<div class="field half">
+										<input type="text" value="" name="LNAME" class="" id="mce-LNAME"
+											placeholder="Last Name">
+									</div>
+									<div class="field">
+										</label>
+										<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL"
+											required placeholder="Email">
+									</div>
+									<div id="mce-responses" class="clear foot">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>
+									<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+									<div style="position: absolute; left: -5000px;" aria-hidden="true"><input
+											type="text" name="b_51ad6738951128de8653576d7_2abe0abca0" tabindex="-1"
+											value=""></div>
+
+								</div>
+
+								<ul class="actions">
+									<li>
+										<input type="submit" value="Subscribe" name="subscribe"
+											id="mc-embedded-subscribe" class="button">
+									</li>
+								</ul>
+							</form>
+						</div>
+
+					</section>
+				</div>
+				<div class="copyright">
+					<p>This website is licensed
+						under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons
+							Attribution-NonCommercial 4.0 International License</a>.</p>
+				</div>
+			</footer>
+
+    `);
